1baa940feb28dce32fe47ecad78ea1cb7f1b8640
Matthew Hough <matthough1@icloud.com>
2021-03-22 12:12:11 -0400

    add test case for requestAnimationFrame

--- src/customRequestAnimationFrame/customRequestAnimationFrame.test.js
+++ src/customRequestAnimationFrame/customRequestAnimationFrame.test.js
@@ -1,1 +1,20 @@
-// TODO: Write tests for new functionality
+import customRequestAnimationFrame from "./customRequestAnimationFrame";
+
+describe("customRequestAnimationFrame", () => {
+   it("invokes callback that specifies delay after non-delay callback", () => {
+      const methodResults = [];
+      const delayText = "delay";
+      const nonDelayText = "no-delay";
+      const nonDelayCallback = () => {
+         methodResults.push(delayText);
+      };
+      const delayCallback = () => {
+         methodResults.push(nonDelayText);
+
+         expect(methodResults[0]).toBe(nonDelayText);
+      };
+
+      customRequestAnimationFrame(delayCallback, 5);
+      customRequestAnimationFrame(nonDelayCallback);
+   });
+});
